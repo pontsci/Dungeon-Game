d4b0631f36a537e5d7070f390fa83433e80baac0
Mason Flemingm <mgfleming@unomaha.edu>
2020-11-06 17:08:47 -0600

    create a script that changes material when a interactable trigger enters

---
+++ Assets/Scripts/ChangeMaterialOnEnterInteract.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterialOnEnterInteract : MonoBehaviour
+{
+
+    public Material interactiveMaterial;
+    public Material defaultMaterial;
+
+    private Renderer rend;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = gameObject.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeToDefaultMaterial()
+    {
+        rend.material = defaultMaterial;
+    }
+
+    public void ChangeToInteractiveMaterial()
+    {
+        rend.material = interactiveMaterial;
+    }
+}
---
+++ Assets/Scripts/Interact.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+
+
+    ChangeMaterialOnEnterInteract changeScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Interactable")
+        {
+            Debug.Log("Detected Interactable!");
+            changeScript = other.gameObject.GetComponent<ChangeMaterialOnEnterInteract>();
+            changeScript.ChangeToInteractiveMaterial();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Interactable")
+        {
+            Debug.Log("Detected Leaving Interactable!");
+            changeScript = other.gameObject.GetComponent<ChangeMaterialOnEnterInteract>();
+            changeScript.ChangeToDefaultMaterial();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Interactable")
+        {
+            
+
+
+        }
+    }
+}
