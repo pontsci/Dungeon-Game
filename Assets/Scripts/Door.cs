99f4efc79feee42bbb41f3f667a3c5c9e5587000
Mason Flemingm <mgfleming@unomaha.edu>
2020-11-07 16:57:17 -0600

    move some variables to door

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,18 +4,33 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    public bool doorMoving { get; set; }
-    public bool doorClosed { get; set; }
+    public bool doorMoving { get; set; } = false;
+    public bool doorClosed { get; set; } = true;
+
+    private RectTransform doorTransform;
     // Start is called before the first frame update
     void Start()
     {
-        
+        doorTransform = gameObject.GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void CloseDoor()
+    {
+        
+        doorClosed = true;
+        doorMoving = false;
+    }
+
+    public void OpenDoor()
+    {
+        doorClosed = false;
+        doorMoving = false;
     }
 
     
--- Assets/Scripts/Lever.cs
+++ Assets/Scripts/Lever.cs
@@ -5,48 +5,34 @@ using UnityEngine;
 public class Lever : MonoBehaviour
 {
     public GameObject door;
-    private RectTransform doorTransform;
-    private bool doorMoving = false;
-    private bool doorClosed = true;
+    public Door doorScript;
 
     // Start is called before the first frame update
     void Start()
     {
-        doorTransform = door.GetComponent<RectTransform>();
+        doorScript = door.GetComponent<Door>();
     }
     private void OnTriggerStay(Collider other)
     {
         if (other.tag == "InteractSphere")
         {
-            if (!doorMoving)
+            if (!doorScript.doorMoving)
             {
                 if (Input.GetAxis("Interact") == 1)
                 {
-                    doorMoving = true;
-                    if (doorClosed)
+                    doorScript.doorMoving = true;
+                    if (doorScript.doorClosed)
                     {
-                        OpenDoor();
+                        Debug.Log("Open the door!");
+                        doorScript.OpenDoor();
                     }
                     else
                     {
-                        CloseDoor();
+                        Debug.Log("Close the door!");
+                        doorScript.CloseDoor();
                     }
                 }
             }
         }
-    }
-
-    void OpenDoor()
-    {
-        Debug.Log("Open the door!");
-        doorClosed = false;
-        doorMoving = false;
-    }
-
-    void CloseDoor()
-    {
-        Debug.Log("Close the door!");
-        doorClosed = true;
-        doorMoving = false;
     }
 }
