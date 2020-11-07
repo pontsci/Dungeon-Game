8259b3ca97658e8bc7731f60bfdbe4c1a356daac
Mason Flemingm <mgfleming@unomaha.edu>
2020-11-07 15:29:37 -0600

    begin door script creation

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool doorMoving { get; set; }
+    public bool doorClosed { get; set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
---
+++ Assets/Scripts/Lever.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    public GameObject door;
+    private RectTransform doorTransform;
+    private bool doorMoving = false;
+    private bool doorClosed = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorTransform = door.GetComponent<RectTransform>();
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "InteractSphere")
+        {
+            if (!doorMoving)
+            {
+                if (Input.GetAxis("Interact") == 1)
+                {
+                    doorMoving = true;
+                    if (doorClosed)
+                    {
+                        OpenDoor();
+                    }
+                    else
+                    {
+                        CloseDoor();
+                    }
+                }
+            }
+        }
+    }
+
+    void OpenDoor()
+    {
+        Debug.Log("Open the door!");
+        doorClosed = false;
+        doorMoving = false;
+    }
+
+    void CloseDoor()
+    {
+        Debug.Log("Close the door!");
+        doorClosed = true;
+        doorMoving = false;
+    }
+}
