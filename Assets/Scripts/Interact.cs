de033ed1d08ee330bab6469614baf36f0c56fbec
Mason Flemingm <mgfleming@unomaha.edu>
2020-11-07 19:02:48 -0600

    finish up events for opening door

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class Interact : MonoBehaviour
 {
-
+    public UnityEvent OnDoInteract = new UnityEvent();
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,14 @@ public class Interact : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("F Pressed!");
+        }
     }
 
 
--- Assets/Scripts/Lever.cs
+++ Assets/Scripts/Lever.cs
@@ -6,31 +6,48 @@ public class Lever : MonoBehaviour
 {
     public GameObject door;
     private Door doorScript;
+    private bool inInteractSphere = false;
 
     // Start is called before the first frame update
     void Start()
     {
         doorScript = door.GetComponent<Door>();
     }
-    private void OnTriggerStay(Collider other)
+
+    private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "InteractSphere")
         {
+            Debug.Log("In Range!");
+            inInteractSphere = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "InteractSphere")
+        {
+            Debug.Log("Out of Range!");
+            inInteractSphere = false;
+        }
+    }
+
+    public void ActivateLever()
+    {
+        if (inInteractSphere)
+        {
             if (!doorScript.doorMoving)
             {
-                if (Input.GetAxis("Interact") == 1)
+                doorScript.doorMoving = true;
+                if (doorScript.doorClosed)
                 {
-                    doorScript.doorMoving = true;
-                    if (doorScript.doorClosed)
-                    {
-                        Debug.Log("Lever says: Open the door!");
-                        doorScript.OpenDoor();
-                    }
-                    else
-                    {
-                        Debug.Log("Lever says: Close the door!");
-                        doorScript.CloseDoor();
-                    }
+                    Debug.Log("Lever says: Open the door!");
+                    doorScript.OpenDoor();
+                }
+                else
+                {
+                    Debug.Log("Lever says: Close the door!");
+                    doorScript.CloseDoor();
                 }
             }
         }
