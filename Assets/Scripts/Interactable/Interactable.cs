d58ccc9d68d3292a32ac122d622c5446234ff908
Mason Flemingm <mgfleming@unomaha.edu>
2020-11-10 03:08:01 -0600

    add Pickup script for items to be picked up, inherits interactable. Moved the material changing methods to the Interactable script.

--- Assets/Scripts/Interactable/Interactable.cs
+++ Assets/Scripts/Interactable/Interactable.cs
@@ -7,14 +7,28 @@ public abstract class Interactable : MonoBehaviour
     //whether we're in the sphere or not
     protected bool inInteractSphere = false;
 
+    public Material interactiveMaterial;
+    public Material defaultMaterial;
+
+    protected Renderer rend;
+
+    protected virtual void Start()
+    {
+        //the renderer
+        rend = gameObject.GetComponent<Renderer>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         //entering the sphere, we set to true that we are in
         if (other.tag == "InteractSphere")
         {
-            Debug.Log("In Range!");
+            //Debug.Log("In Range!");
+            //change material
+            ChangeToInteractiveMaterial();
             inInteractSphere = true;
         }
+
     }
 
     private void OnTriggerExit(Collider other)
@@ -22,9 +36,23 @@ public abstract class Interactable : MonoBehaviour
         //exiting the sphere, we set to false that we are in
         if (other.tag == "InteractSphere")
         {
-            Debug.Log("Out of Range!");
+            //Debug.Log("Out of Range!");
+            //change material
+            ChangeToDefaultMaterial();
             inInteractSphere = false;
         }
+    }
+
+    //change to the default material provided
+    protected void ChangeToDefaultMaterial()
+    {
+        rend.material = defaultMaterial;
+    }
+
+    //change to the interactive material provided
+    protected void ChangeToInteractiveMaterial()
+    {
+        rend.material = interactiveMaterial;
     }
 
     //a method invoked by the unity input manager on "F" key
---
+++ Assets/Scripts/Interactable/Pickup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : Interactable
+{
+
+
+    public override void Activate()
+    {
+        PickupAction();
+        Destroy(gameObject);
+    }
+
+    private void PickupAction()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
