db61f12214d3f9c03ee5650137a8435c07910ea9
Mason Fleming <mgfleming@unomaha.edu>
2020-12-04 19:30:51 -0600

    cleanup unneeded variable, add database variable to inventoryObject

--- Assets/Scriptable Objects/Inventory/Scripts/InventoryObject.cs
+++ Assets/Scriptable Objects/Inventory/Scripts/InventoryObject.cs
@@ -5,25 +5,20 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory System/Inventory")]
 public class InventoryObject : ScriptableObject
 {
+    public ItemDatabaseObject database;
     public List<InventorySlot> Container = new List<InventorySlot>();
 
     public void AddItem(ItemObject item, int amount)
     {
-        bool hasItem = false;
-
         for(int i = 0; i < Container.Count; i++)
         {
             if(Container[i].item == item)
             {
                 Container[i].AddAmount(amount);
-                hasItem = true;
-                break;
+                return;
             }
         }
-        if (!hasItem)
-        {
-            Container.Add(new InventorySlot(item, amount));
-        }
+        Container.Add(new InventorySlot(item, amount));
     }
 }
 
