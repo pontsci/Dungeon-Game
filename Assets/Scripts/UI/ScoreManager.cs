5a5d1716de49729bfa0254df4c7eecc72cf37a81
Emeara <Emeara@users.noreply.github.com>
2020-12-08 06:19:21 -0600

    The score tracker now ticks up by 100 when a player collides with a chest. The polyperfect chest was edited to have the script, so any new chests added should tick the score up when collided with. However, I noticed an issue interacting with food when loading the scene from main menu or reloading the a scene. I tried debugging it for a while and commenting out all my code and it persisted. I'm not sure if this error occured before or after my code.

--- Assets/Scripts/UI/ScoreManager.cs
+++ Assets/Scripts/UI/ScoreManager.cs
@@ -6,35 +6,21 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
-    public int startScore = 0;
-    public int currentScore;
+    public int currentScore = 0;
     private Text scoreDisplay;
     float elapsed = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("In the start method for the ScoreManager");
-        currentScore = startScore;
-        scoreDisplay = GameObject.FindGameObjectWithTag("Score").GetComponent<Text>();
-        //scoreDisplay.text = currentScore.ToString();
-        scoreDisplay.text = "" + currentScore;
-    }
-
-    void Update()
-    {
-        elapsed += Time.deltaTime;
-        if (elapsed >= 5f)
-        {
-            Debug.Log("Five seconds have past, adding 5 to our score");
-            elapsed = elapsed % 5f;
-            addScore(5);
-        }
+        //can comment these out and it should still work. If you do, uncomment the code in addScore.
+        scoreDisplay = GameObject.FindGameObjectWithTag("ScoreNum").GetComponent<Text>();
+        scoreDisplay.text = currentScore.ToString();
     }
 
     public void addScore(int value) {
         currentScore = currentScore + value;
-        scoreDisplay = GameObject.FindGameObjectWithTag("Score").GetComponent<Text>();
+        //scoreDisplay = GameObject.FindGameObjectWithTag("ScoreNum").GetComponent<Text>();
         scoreDisplay.text = currentScore.ToString();
     }
 }
---
+++ Assets/Scripts/World/Objects/Chest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    private ScoreManager scoreManager;
+
+    void Start()
+    {
+        //can comment this out and it should still work. If you do, make sure to uncomment the code in OnTriggerEnter.
+        scoreManager = GameObject.FindGameObjectWithTag("Score").GetComponent<ScoreManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //scoreManager = GameObject.FindGameObjectWithTag("Score").GetComponent<ScoreManager>();
+            scoreManager.addScore(100);
+            Destroy(gameObject);
+        }
+    }
+}
