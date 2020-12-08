f64f9a5c7f9a1acf3200d8993624031210f9cd5e
Emeara <Emeara@users.noreply.github.com>
2020-12-07 18:57:47 -0600

    Added a main, added inventory system devscene_emeara, also ran into a strange bug when loading a scene from the main menu, it disappeared after a while but it is something to keep in mind.

--- Assets/Scripts/SC_MainMenu.cs
+++ Assets/Scripts/SC_MainMenu.cs
@@ -18,7 +18,7 @@ public class SC_MainMenu : MonoBehaviour
     public void PlayNowButton()
     {
         // Play Now Button has been pressed, here you can initialize your game (For example Load a Scene called GameLevel etc.)
-        UnityEngine.SceneManagement.SceneManager.LoadScene("GameLevel");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("DevScene_01");
     }
 
     public void CreditsButton()
