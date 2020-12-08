f62e2888dde751a22b650fb1c8702a496e6964e3
Mason Fleming <mgfleming@unomaha.edu>
2020-12-07 19:14:38 -0600

    update the sword so that it rotate with the camera

---
+++ Assets/Scripts/Player/Weapon/SwordCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordCamera : MonoBehaviour
+{
+
+    private Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gameObject.transform.rotation = new Quaternion(transform.eulerAngles.x, );
+    }
+}
