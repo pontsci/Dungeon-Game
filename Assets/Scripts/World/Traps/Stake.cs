b18511c8467d1ed3864eaa49ac2f3d9efc082856
Emeara <Emeara@users.noreply.github.com>
2020-12-09 04:27:55 -0600

    Added a sound for when the player collides with the stakes. As of right now, we don't hear the sound because the scene immediately switches to the MainMenu after death. To hear the sound, comment out the removeHealth function call in the stake script. Just remember to uncomment it when done.

--- Assets/Scripts/World/Traps/Stake.cs
+++ Assets/Scripts/World/Traps/Stake.cs
@@ -7,20 +7,22 @@ public class Stake : MonoBehaviour
     public TrapObject stakeData;
     private Health playerHealthScript;
 
+    public AudioClip scoreAudio;
+    private AudioSource soundSource;
+
+
+    void Start()
+    {
+        soundSource = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            //Debug.Log("We collided with: " + other);
             playerHealthScript = other.gameObject.GetComponent<Health>();
-            //Debug.Log("Health before: " + playerHealthScript.getHealth());
             playerHealthScript.RemoveHealth(stakeData.removeHealthValue);
-            //Debug.Log("Health after: " + playerHealthScript.getHealth());
-          }
-    }
-
-    private void Update()
-    {
-
+            soundSource.PlayOneShot(scoreAudio, 1.0f);
+        }
     }
 }
