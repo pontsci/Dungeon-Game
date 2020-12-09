67ae0f925623f98ff718875fa450a6e0c1f2f8fa
drewRam <ars3nx@gmail.com>
2020-12-09 02:03:47 -0600

    Added swing audio to enemy actions.

--- Assets/Scripts/Enemy/LookAtPlayer.cs
+++ Assets/Scripts/Enemy/LookAtPlayer.cs
@@ -20,6 +20,8 @@ public class LookAtPlayer : MonoBehaviour
     public Animator anim;
     public bool playerReached = false;
 
+    private bool followLastknownPos = false;
+
 
     void Start()
     {
@@ -29,7 +31,41 @@ public class LookAtPlayer : MonoBehaviour
     private void Update()
     {
         if (playerSighted)
+        {
             playerFound();
+        }
+        //if (followLastknownPos)
+        //    stillFollowing();
+    }
+
+    IEnumerator followBeforeBored()
+    {
+        print("Entered bored state");
+        float counter = 0;
+        float disctractedInSeconds = 5;
+
+        while(counter < disctractedInSeconds)
+        {
+            counter += Time.deltaTime;
+            stillFollowing();
+        }
+
+
+        followLastknownPos = false;
+        yield return null;
+    }
+
+    private void stillFollowing()
+    {
+        Vector3 lookAtPlayer = player.position;
+        lookAtPlayer.y = transform.position.y;
+        transform.LookAt(lookAtPlayer);
+
+        if (Math.Ceiling(Vector3.Distance(transform.position, player.position)) > minDistance)
+        {
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+            anim.SetInteger("condition", 1);
+        }
     }
 
     private void playerFound()
@@ -60,13 +96,17 @@ public class LookAtPlayer : MonoBehaviour
     private void OnTriggerStay(Collider other)
     {
         if (other.transform == player)
+        {
             playerSighted = true;
+            followLastknownPos = true;
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.transform == player)
         {
+            //StartCoroutine(followBeforeBored());
             playerSighted = false;
             playerReached = false;
             anim.SetInteger("condition", 0);
--- Assets/Scripts/Enemy/Skeleton.cs
+++ Assets/Scripts/Enemy/Skeleton.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 
 public class Skeleton : MonoBehaviour
 {
-    public int health = 100;
+    public AudioClip swing;
+    AudioSource audioSource;
+    LookAtPlayer vision;
     // Start is called before the first frame update
     void Start()
     {
+        vision = GameObject.FindGameObjectWithTag("Skeleton").GetComponent<LookAtPlayer>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (vision.playerReached)
+        {
+            audioSource.PlayOneShot(swing, .7F);
+        }
     }
 }
