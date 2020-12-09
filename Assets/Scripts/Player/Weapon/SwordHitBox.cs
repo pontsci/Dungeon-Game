31ac7a47f9ea901004d44413df04a66e5bad404a
jhenery <54946731+jhenery@users.noreply.github.com>
2020-12-09 17:10:23 -0600

    Attempting to debug SwordHitBox

--- Assets/Scripts/Player/Weapon/SwordHitBox.cs
+++ Assets/Scripts/Player/Weapon/SwordHitBox.cs
@@ -9,14 +9,18 @@ public class SwordHitBox : MonoBehaviour
     private Sword action;
     private void Start()
     {
-        skel = GameObject.FindGameObjectWithTag("Skeleton").GetComponent<Skeleton>();
         action = GameObject.FindGameObjectWithTag("PlayerSword").GetComponent<Sword>();
+    }
+    private void Update()
+    {
+        
     }
     private void OnTriggerEnter(Collider other)
     {
         print(action.swinging);
         if (other.tag == ("Skeleton") && action.swinging)
         {
+            skel = other.gameObject;
             print(other.tag);
             skel.damage(80);
         }
