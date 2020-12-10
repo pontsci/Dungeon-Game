4d6cd99ab34d07c28ecc8cacd3d254a97501034b
Mason Fleming <mgfleming@unomaha.edu>
2020-12-10 01:47:27 -0600

    get navmesh components and bake navmesh, create skull agent

---
+++ Assets/Scripts/Enemy/Skull.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Skull : MonoBehaviour
+{
+
+    public Transform goal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
