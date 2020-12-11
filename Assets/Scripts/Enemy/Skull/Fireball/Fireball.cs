3452bb0466b1b9fa7aa0f982ea3f324a7b647f66
Mason Fleming <mgfleming@unomaha.edu>
2020-12-10 19:01:08 -0600

    create fireball spawn point and simple fireball

---
+++ Assets/Scripts/Enemy/Skull/Fireball/Fireball.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    [SerializeField] Vector3 target; //the target we want to fly towards
+
+    public void Start()
+    {
+        
+    }
+
+
+    public void SetTarget(Vector3 target)
+    {
+        this.target = target;
+    }
+}
